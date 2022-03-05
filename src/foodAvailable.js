9d1938c2eafc379d7ef38e56b26e5010413194b9
lo6169 <lo6169@rit.edu>
2022-03-05 17:36:28 -0500

    Added near you search and some fake JSON data

---
+++ src/foodAvailable.js
@@ -0,0 +1,59 @@
+export const foodAvailable = {
+    "14623": [
+        {
+            "name": "Wegman's Marketplace",
+            "address": "650 Hylan Drive",
+            "town": "Rochester",
+            "state": "New York",
+            "zip": "14623",
+            "date": "03/06/2022",
+            "timestart": "5:00 PM",
+            "timeend": "8:00 PM",
+            "food": "Apples, bananas, cucumbers, pizza"
+        },
+        {
+            "name": "McDonald's",
+            "address": "45 Jay Scutti Blvd",
+            "town": "Rochester",
+            "state": "New York",
+            "zip": "14623",
+            "date": "03/06/2022",
+            "timestart": "9:00 PM",
+            "timeend": "11:00 PM",
+            "food": "Burgers, fries, milkshakes"
+        },
+        {
+            "name": "TGI Friday's",
+            "address": "125 White Spruce Blvd",
+            "town": "Rochester",
+            "state": "New York",
+            "zip": "14623",
+            "date": "03/06/2022",
+            "timestart": "7:00 PM",
+            "timeend": "11:00 PM",
+            "food": "Burgers, chicken, steak, chips"
+        },
+        {
+            "name": "Gracie's",
+            "address": "Grace Watson Hall, Greenleaf Ct",
+            "town": "Rochester",
+            "state": "New York",
+            "zip": "14623",
+            "date": "03/06/2022",
+            "timestart": "7:00 PM",
+            "timeend": "11:00 PM",
+            "food": "Nothing edible"
+        },
+        {
+            "name": "Bean's",
+            "address": "Grace Watson Hall, Greenleaf Ct",
+            "town": "Rochester",
+            "state": "New York",
+            "zip": "14623",
+            "date": "03/08/2022",
+            "timestart": "7:00 PM",
+            "timeend": "8:00 PM",
+            "food": "Baked goods such as elephant ears, danishes, muffins, and smoothies!"
+        }
+    ]
+}
--- src/pages/nearyou.js
+++ src/pages/nearyou.js
@@ -1,8 +1,61 @@
-import './home.css';
-
+import './nearyou.css';
+import { foodAvailable } from '../foodAvailable';
+import { useState } from 'react';
+ 
 function NearYou() {
+    const [food, setFood] = useState(null);
+    const [zip, setZip] = useState(null);
+
+    function search(event) {
+        event.preventDefault();
+        setZip(event.target[0].value);
+
+        const newFoodList = foodAvailable[event.target[0].value];
+        setFood(newFoodList);
+        event.target[0].value = "";
+        console.log(newFoodList);
+    };
+
   return (
     <div>
+        <div class="form-style-6">
+            <h1>Find Food Near You</h1>
+            <form onSubmit={search}>
+                <label htmlFor='zip'>Enter your Zip</label>
+                <input id="zip" name="zip" type="text" 
+                inputMode="numeric" placeholder='Zipcode'></input> <br/>
+                <input type="submit" value="Search" ></input>
+            </form>
+        </div>
+
+        <div class="table-wrapper">
+            <table class="fl-table">
+                <thead>
+                <tr>
+                    <th>Name of Establishment</th>
+                    <th>Address</th>
+                    <th>Date</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Food Available</th>
+                </tr>
+                </thead> 
+                <tbody>
+                    {food && 
+                        food.map((info, index) => (
+                            <tr key={`${info} ${index}`}>
+                                <td>{info.name}</td>
+                                <td>{info.address}, {info.town}, {info.state}</td>
+                                <td>{info.date}</td>
+                                <td>{info.timestart}</td>
+                                <td>{info.timeend}</td>
+                                <td>{info.food}</td>
+                            </tr>
+                        ))
+                    } 
+                </tbody>
+            </table>
+        </div>
         
     </div>
   );
